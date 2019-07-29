e7dfa0ffc091bc6df8e51b57a233166604313d2c
Erik Perez <erik.p@prisguide.no>
2019-07-29 15:20:41 +0200

    Changed suggester to be a class (EmailSuggester) with an option to use extra domains

--- index.js
+++ index.js
@@ -2,24 +2,42 @@
  * Dependencies
  */
 
-const distance = require('js-levenshtein')
-const domains = require('email-domain-popular')
+const distance = require("js-levenshtein");
+const domains = require("email-domain-popular");
 
-/**
- * Give suggestion based on levenshtein distance between the given email
- * and a list of popular domains.
- *
- * @param {String} address
- * @return {String}
- * @api public
- */
+class EmailSuggester {
+  constructor(extraDomains) {
+    this.defaultDomains = domains;
+    if (extraDomains && Array.isArray(extraDomains)) {
+      extraDomains.forEach(extra => {
+        this.defaultDomains.push(extra);
+      });
+    }
+  }
 
-module.exports = (address) => {
-  const [left, domain] = address.split('@')
-  if (domain) {
-    const suggestions = domains.sort((a, b) => {
-      return distance(a, domain) - distance(b, domain)
-    })
-    return left + '@' + suggestions[0]
+  suggest(address, maxDistance) {
+    if (typeof maxDistance === "undefined") maxDistance = 5;
+
+    const [left, domain] = address.split("@");
+    if (domain) {
+      const suggestions = this.defaultDomains
+        .map(e => {
+          return { distance: distance(e, domain), email: left + "@" + e };
+        })
+        .filter(e => e.distance < maxDistance)
+        .sort((a, b) => a.distance - b.distance);
+
+      return suggestions;
+    }
   }
 }
+module.exports = EmailSuggester;
+// module.exports = address => {
+//   const [left, domain] = address.split("@");
+//   if (domain) {
+//     const suggestions = domains.sort((a, b) => {
+//       return distance(a, domain) - distance(b, domain);
+//     });
+//     return left + "@" + suggestions[0];
+//   }
+// };
--- test/email-domain-levenshtein.js
+++ test/email-domain-levenshtein.js
@@ -2,16 +2,83 @@
   Dependencies
  */
 
-const test = require('tape')
-const suggest = require('..')
+const test = require("tape");
+const EmailSuggester = require("../index.js");
+es = new EmailSuggester([
+  "online.no",
+  "live.no",
+  "live.com",
+  "icloud.com",
+  "yahoo.no",
+  "lyse.net",
+  "msn.com",
+  "me.com"
+]);
 
+let testCounter = 1;
 
-test('should suggest email #1', assert => {
-  assert.plan(1)
-  assert.equal(suggest('hello@gnail.com'), 'hello@gmail.com')
-})
+test("should suggest email #" + testCounter++, assert => {
+  assert.plan(1);
+  let suggestions = es.suggest("hello@gnail.no");
+  assert.equal(suggestions[0].email, "hello@gmail.com");
+});
 
-test('should suggest email #2', assert => {
-  assert.plan(1)
-  assert.equal(suggest('hello@yahop.com'), 'hello@yahoo.com')
-})
+test("should suggest email #" + testCounter++, assert => {
+  assert.plan(1);
+  let suggestions = es.suggest("hello@yahop.no");
+  assert.equal(suggestions[0].email, "hello@yahoo.no");
+});
+
+test("should suggest email #" + testCounter++, assert => {
+  assert.plan(1);
+  let suggestions = es.suggest("hello@yahuu.no");
+  assert.equal(suggestions[0].email, "hello@yahoo.no");
+});
+
+test("should suggest email #" + testCounter++, assert => {
+  assert.plan(1);
+  let suggestions = es.suggest("hello@onlin.no");
+  assert.equal(suggestions[0].email, "hello@online.no");
+});
+
+test("should suggest email #" + testCounter++, assert => {
+  assert.plan(1);
+  let suggestions = es.suggest("hello@gmali.no");
+  assert.equal(suggestions[0].email, "hello@gmail.com");
+});
+
+test("should suggest email #" + testCounter++, assert => {
+  assert.plan(1);
+  let suggestions = es.suggest("hello@outlok.com");
+  assert.equal(suggestions[0].email, "hello@outlook.com");
+});
+
+test("should suggest email #" + testCounter++, assert => {
+  assert.plan(1);
+  let suggestions = es.suggest("hello@iclod.no");
+  assert.equal(suggestions[0].email, "hello@icloud.com");
+});
+
+test("should suggest email #" + testCounter++, assert => {
+  assert.plan(1);
+  let suggestions = es.suggest("hello@lysr.net");
+  assert.equal(suggestions[0].email, "hello@lyse.net");
+});
+
+test("should suggest email #" + testCounter++, assert => {
+  assert.plan(1);
+  let suggestions = es.suggest("hello@msn.no");
+  assert.equal(suggestions[0].email, "hello@msn.com");
+});
+
+test("should suggest email #" + testCounter++, assert => {
+  assert.plan(1);
+  let suggestions = es.suggest("hello@me.no");
+  assert.equal(suggestions[0].email, "hello@me.com");
+});
+
+test("should suggest email #" + testCounter++, assert => {
+  assert.plan(1);
+  let suggestions = es.suggest("hello@md.com");
+  assert.equal(suggestions[0].email, "hello@me.com");
+});
